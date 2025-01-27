93459d98d01500f8b2be7019ee57ccdef9252cee
Nasiruddin <143922021+nasiruddinkhatib@users.noreply.github.com>
2025-01-28 00:49:29 +0530

    Created a class

---
+++ First_class.py
@@ -0,0 +1,11 @@
+class Phone:  # Class Definition
+    def make_call(self):  # Method to make a call
+        print("Making a call")  # Prints a message indicating a call is being made
+
+    def play_game(self):  # Method to play a game
+        print("Playing Game")  # Prints a message indicating a game is being played
+
+p1 = Phone()  # Object instantiation of the Phone class
+p1.make_call()  # Calls the make_call method on the Phone object
+p1.play_game()  # Calls the play_game method on the Phone object
+
