e6bd1bd67730595b4280d02efa031cd3dbfb1a06
Nasiruddin <143922021+nasiruddinkhatib@users.noreply.github.com>
2025-02-15 00:29:21 +0530

    First_class.py

--- First_class.py
+++ First_class.py
@@ -1,3 +1,7 @@
+#****----**********---****Class in python/OOP******----------*********-**--*-*
+# To make Class we use keyword Class try to keep name of class in CamelCase so easily we'll know that it is a class 
+# we call class using objectand also  we can create many object to call Class and
+# by making class we call Functions/methods and Variable 
 class Phone:  # Class Definition
     def make_call(self):  # Method to make a call
         print("Making a call")  # Prints a message indicating a call is being made
