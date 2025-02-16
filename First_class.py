389b64a607fcc7d043fff181ec38c36b94465cb6
Nasiruddin <143922021+nasiruddinkhatib@users.noreply.github.com>
2025-02-16 23:45:35 +0530

    First_class.py

--- First_class.py
+++ First_class.py
@@ -1,7 +1,9 @@
-#****----**********---****Class in python/OOP******----------*********-**--*-*
+#**************--------------**********Class in python/OOP***************-------------*************-----------*******
 # To make Class we use keyword Class try to keep name of class in CamelCase so easily we'll know that it is a class 
 # we call class using objectand also  we can create many object to call Class and
 # by making class we call Functions/methods and Variable 
+#*****************------------------**********************----------------********************---------**********
+##Example 1 
 class Phone:  # Class Definition
     def make_call(self):  # Method to make a call
         print("Making a call")  # Prints a message indicating a call is being made
@@ -12,4 +14,13 @@ class Phone:  # Class Definition
 p1 = Phone()  # Object instantiation of the Phone class
 p1.make_call()  # Calls the make_call method on the Phone object
 p1.play_game()  # Calls the play_game method on the Phone object
+#******************-----------------********************---------------****************-------------***************
+##Example 2
+class Animal:       # class Name 
+    def speak(self):  #function
+        print("Animal makes a sound")  
+dog = Animal()      # Creating an object of the Animal class
+dog.speak() 
+# Output: Animal makes a sound
+
 
