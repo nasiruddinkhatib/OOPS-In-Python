ae7522749072868762b4cf93dffe42cdee3db93f
Nasir <143922021+nasiruddinkhatib@users.noreply.github.com>
2025-04-19 07:59:25 +0530

    Hierarchical.py

--- Hierarchical.py
+++ Hierarchical.py
@@ -34,3 +34,35 @@ m1.show_role()   # ✅ Specific to Manager
 
 d1.show_name()   # ✅ Inherited from Employee
 d1.show_role()   # ✅ Specific to Developer
+
+#****************----------------*********************------------------******************----------*****************
+#Simple Example Using Class Method and static method 
+class A:
+    a = 10  # Class attribute 'a' initialized to 10
+    b = 20  # Class attribute 'b' initialized to 20
+
+    def __init__(self, c, d):
+        self.c = c  # Instance attribute 'c' initialized with the value passed during object creation
+        self.d = d  # Instance attribute 'd' initialized with the value passed during object creation
+
+class B(A):  # Class 'B' inheriting from class 'A'
+    a = 100  # Class attribute 'a' in 'B' overrides the 'a' from class 'A'
+
+    @classmethod
+    def display(cls):  # Class method 'display' that takes the class itself as the first argument 'cls'
+        print(cls.a)  # Accessing the class attribute 'a' of the current class ('B' in this case)
+
+class C(A):  # Class 'C' inheriting from class 'A'
+    m = 3000  # Class attribute 'm' initialized to 3000
+
+    @staticmethod
+    def msg():  # Static method 'msg' that does not take 'self' or 'cls' as an argument
+        print("heirarchical inheritance ")  # Prints a message
+
+print(A.a, A.b)  # Accessing class attributes 'a' and 'b' directly using the class name 'A'
+print(B.a, B.b)  # Accessing class attribute 'a' from class 'B' (which overrides 'A.a') and 'b' from class 'A' through inheritance
+print(C.a, C.b, C.m)  # Accessing class attributes 'a' and 'b' from class 'A' through inheritance, and 'm' which is a class attribute of 'C'
+#Output:
+# 10 20
+# 100 20
+# 10 20 3000
